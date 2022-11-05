b8c003d1e41c66880f0090a7bb3f09f9c45e9651
Albert Molano <aa.molano@uniandes.edu.co>
2022-11-05 14:40:41 -0500

    Feat: Bug-Tracking

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:universities/presentation/routes/routes.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]).then((_) {
-    runApp(const MyApp());
-  });
+
+  await SentryFlutter.init((options) {
+    options.dsn =
+        'https://67987282a90b4289a6d8d5d02f4c3ef9@o4504108157829120.ingest.sentry.io/4504108158812160';
+    options.tracesSampleRate = 1.0;
+  },
+      appRunner: () => SystemChrome.setPreferredOrientations([
+            DeviceOrientation.portraitUp,
+          ]).then((_) {
+            runApp(const MyApp());
+          }));
 }
 
 class MyApp extends StatelessWidget {
